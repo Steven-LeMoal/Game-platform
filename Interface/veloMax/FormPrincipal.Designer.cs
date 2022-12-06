2f2f0e36c503e2de438af65c247d7b4ac58a2508
Steven <110969196+Steven-LeMoal@users.noreply.github.com>
2022-12-07 00:49:15 +0100

    Add files via upload

---
+++ Interface/veloMax/FormPrincipal.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace veloMax
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelSystemeSubMenu = new System.Windows.Forms.Panel();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.btnSysteme = new System.Windows.Forms.Button();
+            this.btnCommandes = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.userInfo = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelChildForm = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelSystemeSubMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelChildForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.AutoScroll = true;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.panelMenu.Controls.Add(this.panelSystemeSubMenu);
+            this.panelMenu.Controls.Add(this.btnSysteme);
+            this.panelMenu.Controls.Add(this.btnCommandes);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(250, 897);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMenu_Paint);
+            // 
+            // panelSystemeSubMenu
+            // 
+            this.panelSystemeSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.panelSystemeSubMenu.Controls.Add(this.button13);
+            this.panelSystemeSubMenu.Controls.Add(this.button14);
+            this.panelSystemeSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSystemeSubMenu.Location = new System.Drawing.Point(0, 279);
+            this.panelSystemeSubMenu.Name = "panelSystemeSubMenu";
+            this.panelSystemeSubMenu.Size = new System.Drawing.Size(250, 84);
+            this.panelSystemeSubMenu.TabIndex = 16;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.button13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button13.FlatAppearance.BorderSize = 0;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button13.ForeColor = System.Drawing.Color.LightGray;
+            this.button13.Location = new System.Drawing.Point(0, 40);
+            this.button13.Name = "button13";
+            this.button13.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button13.Size = new System.Drawing.Size(250, 40);
+            this.button13.TabIndex = 1;
+            this.button13.Text = "Utilisateurs";
+            this.button13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click_1);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.button14.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button14.FlatAppearance.BorderSize = 0;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button14.ForeColor = System.Drawing.Color.LightGray;
+            this.button14.Location = new System.Drawing.Point(0, 0);
+            this.button14.Name = "button14";
+            this.button14.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button14.Size = new System.Drawing.Size(250, 40);
+            this.button14.TabIndex = 0;
+            this.button14.Text = "Jeux";
+            this.button14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click_1);
+            // 
+            // btnSysteme
+            // 
+            this.btnSysteme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnSysteme.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSysteme.FlatAppearance.BorderSize = 0;
+            this.btnSysteme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSysteme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSysteme.ForeColor = System.Drawing.Color.White;
+            this.btnSysteme.Location = new System.Drawing.Point(0, 234);
+            this.btnSysteme.Name = "btnSysteme";
+            this.btnSysteme.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnSysteme.Size = new System.Drawing.Size(250, 45);
+            this.btnSysteme.TabIndex = 15;
+            this.btnSysteme.Text = "Système";
+            this.btnSysteme.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSysteme.UseVisualStyleBackColor = false;
+            this.btnSysteme.Click += new System.EventHandler(this.btnSysteme_Click_1);
+            // 
+            // btnCommandes
+            // 
+            this.btnCommandes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnCommandes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCommandes.FlatAppearance.BorderSize = 0;
+            this.btnCommandes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCommandes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCommandes.ForeColor = System.Drawing.Color.White;
+            this.btnCommandes.Location = new System.Drawing.Point(0, 189);
+            this.btnCommandes.Name = "btnCommandes";
+            this.btnCommandes.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnCommandes.Size = new System.Drawing.Size(250, 45);
+            this.btnCommandes.TabIndex = 5;
+            this.btnCommandes.Text = "Jeux";
+            this.btnCommandes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCommandes.UseVisualStyleBackColor = false;
+            this.btnCommandes.Click += new System.EventHandler(this.btnJeux_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.userInfo);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(250, 189);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // userInfo
+            // 
+            this.userInfo.BackColor = System.Drawing.Color.Black;
+            this.userInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.userInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.userInfo.Enabled = false;
+            this.userInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.userInfo.ForeColor = System.Drawing.Color.Gainsboro;
+            this.userInfo.HideSelection = false;
+            this.userInfo.Location = new System.Drawing.Point(36, 129);
+            this.userInfo.Multiline = true;
+            this.userInfo.Name = "userInfo";
+            this.userInfo.ReadOnly = true;
+            this.userInfo.Size = new System.Drawing.Size(161, 33);
+            this.userInfo.TabIndex = 1;
+            this.userInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.userInfo.TextChanged += new System.EventHandler(this.user_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::veloMax.Properties.Resources.icon;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelChildForm
+            // 
+            this.panelChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.panelChildForm.Controls.Add(this.panel4);
+            this.panelChildForm.Controls.Add(this.panel3);
+            this.panelChildForm.Controls.Add(this.panel2);
+            this.panelChildForm.Controls.Add(this.panel1);
+            this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm.ForeColor = System.Drawing.Color.Black;
+            this.panelChildForm.Location = new System.Drawing.Point(250, 0);
+            this.panelChildForm.Name = "panelChildForm";
+            this.panelChildForm.Size = new System.Drawing.Size(1594, 897);
+            this.panelChildForm.TabIndex = 1;
+            this.panelChildForm.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.AutoSize = true;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(1594, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(0, 897);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSize = true;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1594, 0);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.AutoSize = true;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 897);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1594, 0);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 897);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // panel5
+            // 
+            this.panel5.AutoSize = true;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(250, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(0, 897);
+            this.panel5.TabIndex = 2;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.AutoSize = true;
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel6.Location = new System.Drawing.Point(250, 897);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(1594, 0);
+            this.panel6.TabIndex = 3;
+            // 
+            // panel8
+            // 
+            this.panel8.AutoSize = true;
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel8.Location = new System.Drawing.Point(1844, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(0, 897);
+            this.panel8.TabIndex = 5;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(1844, 897);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panelChildForm);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panelMenu);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1400, 900);
+            this.Name = "FormPrincipal";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "StevenINTERFACE by LANS";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelSystemeSubMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelChildForm.ResumeLayout(false);
+            this.panelChildForm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelChildForm;
+        private System.Windows.Forms.TextBox userInfo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCommandes;
+        private System.Windows.Forms.Panel panelSystemeSubMenu;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button btnSysteme;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel8;
+    }
+}
+
